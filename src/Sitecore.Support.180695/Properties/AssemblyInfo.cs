dd31a2872750cc2f35d26433f22ce27081b9c0b3
DK\mao <mao@sitecore.net>
2017-09-22 18:30:34 +0300

    Prevent setting SessionStateBehavior to ReadOnly mode.

---
+++ src/Sitecore.Support.180695/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.180695")]
+[assembly: AssemblyProduct("Sitecore.Support.180695")]
+[assembly: ComVisible(false)]
